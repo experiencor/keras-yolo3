90ce9b92faf1481182d57556cf8205e8dcac5f67
kchwang <clement10601@gmail.com>
2018-11-27 11:13:29 +0800

    add voc boxpoints parser (x1,y1,x2,y2,x3,y3,x4,y4)

--- voc.py
+++ voc.py
@@ -65,3 +65,80 @@ def parse_voc_annotation(ann_dir, img_dir, cache_name, labels=[]):
             pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)    
                         
     return all_insts, seen_labels
+
+def parse_boxpoints_annotation(ann_dir, img_dir, cache_name, labels=[]):
+    if os.path.exists(cache_name):
+        with open(cache_name, 'rb') as handle:
+            cache = pickle.load(handle)
+        all_insts, seen_labels = cache['all_insts'], cache['seen_labels']
+    else:
+        all_insts = []
+        seen_labels = {}
+        
+        for ann in sorted(os.listdir(ann_dir)):
+            img = {'object':[]}
+
+            try:
+                tree = ET.parse(ann_dir + ann)
+            except Exception as e:
+                print(e)
+                print('Ignore this bad annotation: ' + ann_dir + ann)
+                continue
+            
+            for elem in tree.iter():
+                if 'filename' in elem.tag:
+                    img['filename'] = img_dir + elem.text
+                if 'width' in elem.tag:
+                    img['width'] = int(elem.text)
+                if 'height' in elem.tag:
+                    img['height'] = int(elem.text)
+                if 'object' in elem.tag or 'part' in elem.tag:
+                    obj = {}
+                    
+                    for attr in list(elem):
+                        if 'name' in attr.tag:
+                            obj['name'] = attr.text
+
+                            if obj['name'] in seen_labels:
+                                seen_labels[obj['name']] += 1
+                            else:
+                                seen_labels[obj['name']] = 1
+                            
+                            if len(labels) > 0 and obj['name'] not in labels:
+                                break
+                            else:
+                                img['object'] += [obj]
+                                
+                        if 'bndbox' in attr.tag:
+                            for dim in list(attr):
+                                if 'x0' in dim.tag:
+                                    obj['x0'] = int(round(float(dim.text)))
+                                else:
+                                    if 'x4' in dim.tag:
+                                        obj['x4'] = int(round(float(dim.text)))
+                                if 'y0' in dim.tag:
+                                    obj['y0'] = int(round(float(dim.text)))
+                                else:
+                                    if 'y4' in dim.tag:
+                                        obj['y4'] = int(round(float(dim.text)))
+                                if 'x1' in dim.tag:
+                                    obj['x1'] = int(round(float(dim.text)))
+                                if 'y1' in dim.tag:
+                                    obj['y1'] = int(round(float(dim.text)))
+                                if 'x2' in dim.tag:
+                                    obj['x2'] = int(round(float(dim.text)))
+                                if 'y2' in dim.tag:
+                                    obj['y2'] = int(round(float(dim.text)))
+                                if 'x3' in dim.tag:
+                                    obj['x3'] = int(round(float(dim.text)))
+                                if 'y3' in dim.tag:
+                                    obj['y3'] = int(round(float(dim.text)))
+
+            if len(img['object']) > 0:
+                all_insts += [img]
+
+        cache = {'all_insts': all_insts, 'seen_labels': seen_labels}
+        with open(cache_name, 'wb') as handle:
+            pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)    
+                        
+    return all_insts, seen_labels
